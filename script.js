43afdb9d87ab285dcd013e6036d5d486f740618c
LilitK <karapetyanlil01@gmail.com>
2021-11-13 22:52:44 -0800

    update

--- script.js
+++ script.js
@@ -1,245 +1,220 @@
-let board = document.getElementById('board');
-let modal = document.getElementById("myModal");
-let buttons = document.getElementById("buttons");
-let span = document.getElementsByClassName("close")[0];
-let boardSize;
-let matrix = [];
-let tempMatrix = [];
-let rabbit;
-let home;
-let wolfPoses = [];
-
-function loadGame(elem) {
-    a = elem.id;
-    boardSize = a;
-    buttons.style.display = "none";
-    create();
+let board = new Array(), boardUi = new Array(), boardFind = new Array();
+let boardSize, wolfCount, fenceCount;
+let win = false, move = true;
+const characters = {
+    wolf: {
+        name: "wolf",
+        id: 1,
+        count: wolfCount,
+        position: []
+    },
+    home: {
+        name: "home",
+        id: 3,
+        count: 1,
+        position: []
+    },
+    rabbit: {
+        name: "rabbit",
+        id: 5,
+        count: 1,
+        position: []
+    },
+    fence: {
+        name: "fence",
+        id: 2,
+        count: fenceCount,
+        position: []
+    },
+    stone: {
+        name: "stone",
+        id: 2,
+        count: fenceCount,
+        position: []
+    }
 }
 
-span.onclick = function () {
-    modal.style.display = "none";
-    window.location.reload();
+function startGame(elem) {
+    createEmptyBoard(elem)
+    positionPlayers()
 }
 
-function create() {
+function createEmptyBoard(elem) {
+    boardSize = elem.id;
+    wolfCount = fenceCount = boardSize / 2;
+    document.getElementById("buttons").style.display = "none";
     for (let i = 0; i < boardSize; i++) {
-        let tr = document.createElement('tr');
-        tr.classList.add("mytr");
-        matrix[i] = [];
+        board[i] = new Array();
+        boardUi[i] = document.createElement('tr');
         for (let j = 0; j < boardSize; j++) {
-            let td = document.createElement('td');
-            td.setAttribute("id", "0")
-
-            td.classList.add("mytd");
-            matrix[i][j] = '.';
-            tr.appendChild(td);
+            board[i][j] = 0;
+            boardUi[i][j] = document.createElement('td');
+            boardUi[i].appendChild(boardUi[i][j]);
         }
-        board.appendChild(tr);
+        document.getElementById('board').appendChild(boardUi[i]);
     }
+}
 
-    let all = document.querySelectorAll(".mytd");
-    let k = 0;
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix.length; j++) {
-            matrix[i][j] = all[k];
-            k++;
+function positionPlayers() {
+    positionCharacter(board, characters.rabbit, 1)
+    positionCharacter(board, characters.home, 1)
+    positionCharacter(board, characters.wolf, wolfCount)
+    positionCharacter(board, characters.fence, fenceCount)
+    positionCharacter(board, characters.stone, fenceCount)
+}
+
+function positionCharacter(board, character, count) {
+    for (let i = 0; i < count; i++) {
+        positionSingleCharacter(board, character);
+    }
+}
+
+function positionSingleCharacter(board, character) {
+    const [x, y] = getRandomFreeCoords(board);
+    board[x][y] = character.id;
+    boardUi[x][y].classList.add(character.name);
+    character.position.push({ x: x, y: y });
+}
+
+function getRandomFreeCoords(board) {
+    const [x, y] = [getRandomCoords(boardSize), getRandomCoords(boardSize)];
+    if (board[x][y] === 0) {
+        return [x, y];
+    }
+    return getRandomFreeCoords(board);
+}
+
+function getRandomCoords(boardSize) {
+    let rand = Math.floor(Math.random() * (boardSize - 1));
+    return rand;
+}
+
+function reactOnKeyboard(direction) {
+    changeRabbitPosiotion(direction)
+    if (move) {
+        for (let i = 0; i < wolfCount; i++) {
+            let wolfCoord = Object.values(characters.wolf.position[i]);
+            let rabbitCoord = Object.values(characters.rabbit.position[0]);
+            attackRabbit(wolfCoord, rabbitCoord, i);
         }
     }
+}
 
-    let x = Math.floor(Math.random() * boardSize);
-    let y = Math.floor(Math.random() * boardSize);
-    rabbit = matrix[x][y];
-    rabbit.classList.add("rabbit");
-    let rabbitPos = {
-        x: x,
-        y: y
-    };
-
-    let homeX = Math.floor(Math.random() * boardSize);
-    let homeY = Math.floor(Math.random() * boardSize);
-    if (!matrix[homeX][homeY].classList.contains('rabbit')) {
-        home = matrix[homeX][homeY];
-        home.classList.add("home");
-        matrix[homeX][homeX].setAttribute("id", "1");
+document.onkeydown = reactOnKeyboard;
+function changeRabbitPosiotion(direction) {
+    [{ x, y }] = characters.rabbit.position;
+    let newRabbitX = x, newRabbitY = y;
+    switch (direction.code) {
+        case "ArrowLeft":
+            newRabbitY = y > 0 ? y - 1 : nextPos = boardSize - 1;
+            moveRabbit(x, newRabbitY);
+            break;
+        case "ArrowRight":
+            newRabbitY = y < boardSize - 1 ? y + 1 : nextPos = 0;
+            moveRabbit(x, newRabbitY);
+            break;
+        case "ArrowUp":
+            newRabbitX = x > 0 ? x - 1 : nextPos = boardSize - 1;
+            moveRabbit(newRabbitX, y);
+            break;
+        case "ArrowDown":
+            newRabbitX = x < boardSize - 1 ? x + 1 : nextPos = 0;
+            moveRabbit(newRabbitX, y);
+            break;
+        default: move = false;
+            break;
     }
+}
 
-    let fenceNum = boardSize - 1;
-    do {
-        let x = Math.floor(Math.random() * boardSize);
-        let y = Math.floor(Math.random() * boardSize);
+function moveRabbit(newRabbitX, newRabbitY) {
+    move = true;
+    if (board[newRabbitX][newRabbitY] === 0 || board[newRabbitX][newRabbitY] === characters.home.id) {
+        board[x][y] = 0;
+        boardUi[x][y].classList.remove(characters.rabbit.name);
+        if (boardUi[newRabbitX][newRabbitY].classList.contains(characters.home.name)) {
+            win = true;
+            gameOver(x, y, win);
+            move = false;
+            return;
+        }
+        boardUi[newRabbitX][newRabbitY].classList.add(characters.rabbit.name);
+        x = newRabbitX; y = newRabbitY;
+        characters.rabbit.position = [{ x, y }];
+    }
+}
 
-        if (!matrix[x][y].classList.contains('wolf') && !matrix[x][y].classList.contains('home') &&
-            !matrix[x][y].classList.contains('rabbit') && !matrix[x][y].classList.contains('fence')) {
-            matrix[x][y].classList.add("fence");
-            matrix[x][y].setAttribute("id", "1")
+function attackRabbit(wolfCoord, rabbitCoord, i) {
+    let nextCoord = selectMinimumDistanceMove(wolfCoord, rabbitCoord, i);
+    moveWolf(wolfCoord, nextCoord, i);
+}
 
-            let rand = Math.round(Math.random());
-            if (rand == 0) {
-                matrix[x][y].classList.add("wall");
+function selectMinimumDistanceMove(position, end, j) {
+    for (let i = 0; i < boardSize; i++) {
+        boardFind[i] = [];
+        for (let j = 0; j < boardSize; j++) {
+            if (board[i][j] != 0) {
+                boardFind[i][j] = 1;
+            } else {
+                boardFind[i][j] = 0;
             }
-            fenceNum--;
         }
     }
-    while (fenceNum > 0);
-
-    let wolfCount = boardSize - 3;
-    do {
-        let x = Math.floor(Math.random() * boardSize);
-        let y = Math.floor(Math.random() * boardSize);
-
-        if (!matrix[x][y].classList.contains('wolf') && !matrix[x][y].classList.contains('home') &&
-            !matrix[x][y].classList.contains('rabbit') && !matrix[x][y].classList.contains('fence')) {
-            matrix[x][y].classList.add("wolf");
-            matrix[x][y].setAttribute("id", "0")
-
-            wolfPoses.push({
-                x: x,
-                y: y
-            });
-
-            wolfCount--;
-        }
-    }
-    while (wolfCount > 0);
-
-    function win(rabbitPos) {
-        matrix[rabbitPos.x][rabbitPos.y].classList.remove("rabbit");
-        for (let i = 0; i < matrix.length; i++) {
-            for (let j = 0; j < matrix.length; j++) {
-                matrix[i][j] = 0;
+    let visited = [];
+    boardFind[position[0]][position[1]] = 1;
+    visited.push([position]);
+    let path;
+    while (visited.length > 0) {
+        path = visited.shift();
+        let coord = path[path.length - 1];
+        let direcTo = [
+            [coord[0] + 1, coord[1]], [coord[0], coord[1] + 1],
+            [coord[0] - 1, coord[1]], [coord[0], coord[1] - 1]
+        ];
+        for (let i = 0; i < direcTo.length; i++) {
+            if (direcTo[i][0] == end[0] && direcTo[i][1] == end[1]) {
+                path.concat([end]);
+                if (path.length > 1) {
+                    return path[1];
+                } else {
+                    return end;
+                }
             }
+            if (direcTo[i][0] < 0 || direcTo[i][0] >= boardFind.length
+                || direcTo[i][1] < 0 || direcTo[i][1] >= boardFind[0].length
+                || boardFind[direcTo[i][0]][direcTo[i][1]] != 0) {
+                continue;
+            }
+            boardFind[direcTo[i][0]][direcTo[i][1]] = 1;
+            visited.push(path.concat([direcTo[i]]));
         }
-        modal.style.display = "block";
+    }
+    return path[0];
+}
+
+function moveWolf(wolfCoord, nextCoord, i) {
+    board[wolfCoord[0]][wolfCoord[1]] = 0;
+    boardUi[wolfCoord[0]][wolfCoord[1]].classList.remove(characters.wolf.name);
+    [characters.wolf.position[i].x, characters.wolf.position[i].y] = nextCoord;
+    board[characters.wolf.position[i].x][characters.wolf.position[i].y] = 1;
+    boardUi[characters.wolf.position[i].x][characters.wolf.position[i].y].classList.add(characters.wolf.name);
+    if (boardUi[x][y].classList.contains(characters.wolf.name)) {
+        win = false;
+        gameOver(x, y, win);
+        move = false;
+    }
+}
+
+function gameOver(x, y, win) {
+    boardUi[x][y].classList.remove(characters.rabbit.name);
+    document.onkeydown = null;
+    document.getElementById("myModal").style.display = "block";
+    if (win) {
         text1.innerHTML = "You Won!";
+    } else {
+        text1.innerHTML = "You Lose!";
     }
+}
 
-
-    function lose(rabbitPos) {
-        matrix[rabbitPos.x][rabbitPos.y].classList.remove("rabbit");
-        for (let i = 0; i < matrix.length; i++) {
-            for (let j = 0; j < matrix.length; j++) {
-                matrix[i][j] = 0;
-            }
-        }
-        modal.style.display = "block";
-        text1.innerHTML = "Game over!";
-    }
-
-    function moveHorizontal(newRabbitPos) {
-        if (!matrix[rabbitPos.x][newRabbitPos].classList.contains("wolf") &&
-            !matrix[rabbitPos.x][newRabbitPos].classList.contains("fence")) {
-            matrix[rabbitPos.x][rabbitPos.y].classList.remove("rabbit");
-            matrix[rabbitPos.x][newRabbitPos].classList.add("rabbit");
-            rabbitPos.y = newRabbitPos;
-        }
-    }
-
-    function moveVertical(newRabbitPos) {
-        if (!matrix[newRabbitPos][rabbitPos.y].classList.contains("wolf") &&
-            !matrix[newRabbitPos][rabbitPos.y].classList.contains("fence")) {
-            matrix[rabbitPos.x][rabbitPos.y].classList.remove("rabbit");
-            matrix[newRabbitPos][rabbitPos.y].classList.add("rabbit");
-            rabbitPos.x = newRabbitPos;
-        }
-    }
-
-    function findWay(position, end) {
-        let visited = [];
-        tempMatrix[position[0]][position[1]] = 1;
-        visited.push([position]);
-
-        while (visited.length > 0) {
-            let path = visited.shift();
-            let coordinate = path[path.length - 1];
-            let direcTo = [
-                [coordinate[0] + 1, coordinate[1]],
-                [coordinate[0], coordinate[1] + 1],
-                [coordinate[0] - 1, coordinate[1]],
-                [coordinate[0], coordinate[1] - 1]
-            ];
-
-            for (let i = 0; i < direcTo.length; i++) {
-                if (direcTo[i][0] == end[0] && direcTo[i][1] == end[1]) {
-                    path.concat([end]);
-                    if (path.length > 1) {
-                        return path[1];
-                    } else {
-                        return end;
-                    }
-                }
-
-                if (direcTo[i][0] < 0 || direcTo[i][0] >= tempMatrix.length
-                    || direcTo[i][1] < 0 || direcTo[i][1] >= tempMatrix[0].length
-                    || tempMatrix[direcTo[i][0]][direcTo[i][1]] != 0) {
-                    continue;
-                }
-                tempMatrix[direcTo[i][0]][direcTo[i][1]] = 1;
-                visited.push(path.concat([direcTo[i]]));
-            }
-        }
-        return path[0];
-    }
-
-    function moveWolves(rabPos, wolfPos) {
-        tempMatrix = [];
-        for (let i = 0; i < boardSize; i++) {
-            tempMatrix[i] = [];
-            for (let j = 0; j < boardSize; j++) {
-                if (matrix[i][j].classList.contains("fence") ||
-                    matrix[i][j].classList.contains("home") ||
-                    matrix[i][j].classList.contains("wolf")) {
-                    tempMatrix[i][j] = 1;
-                }
-                else {
-                    tempMatrix[i][j] = 0;
-                }
-            }
-        }
-        let currentPos = Object.values(wolfPos);
-        let path = findWay(currentPos, rabPos);
-
-        matrix[currentPos[0]][currentPos[1]].classList.remove("wolf");
-        matrix[path[0]][path[1]].classList.add("wolf");
-
-        wolfPos.x = path[0];
-        wolfPos.y = path[1];
-    }
-
-    document.body.addEventListener('keydown', function (event) {
-        let newRabbitPos;
-        switch (event.key) {
-            case "ArrowLeft":
-                newRabbitPos = rabbitPos.y > 0 ? rabbitPos.y - 1 : nextPos = boardSize - 1;
-                moveHorizontal(newRabbitPos);
-                break;
-            case "ArrowRight":
-                newRabbitPos = rabbitPos.y < boardSize - 1 ? rabbitPos.y + 1 : nextPos = 0;
-                moveHorizontal(newRabbitPos);
-                break;
-            case "ArrowUp":
-                newRabbitPos = rabbitPos.x > 0 ? rabbitPos.x - 1 : nextPos = boardSize - 1;
-                moveVertical(newRabbitPos);
-                break;
-            case "ArrowDown":
-                newRabbitPos = rabbitPos.x < boardSize - 1 ? rabbitPos.x + 1 : nextPos = 0;
-                moveVertical(newRabbitPos);
-                break;
-            default: break;
-        }
-
-        if (matrix[rabbitPos.x][rabbitPos.y].classList.contains("home")) {
-            win(rabbitPos);
-        }
-        else {
-            let rabPos = Object.values(rabbitPos);
-            for (let k = 0; k < wolfPoses.length; k++) {
-                moveWolves(rabPos, wolfPoses[k]);
-
-                if (matrix[rabbitPos.x][rabbitPos.y].classList.contains("wolf")) {
-                    lose(rabbitPos);
-                }
-            }
-        }
-    });
-
+document.getElementsByClassName("close")[0].onclick = function () {
+    window.location.reload();
 }
